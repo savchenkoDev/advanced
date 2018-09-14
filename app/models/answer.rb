d6308aa66371907343af60b3af229cafab57c03a
savchenko <mr.syava1@gmail.com>
2018-09-14 19:44:59 +0500

    Correction v1.2(finish)

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,5 @@
 class Answer < ApplicationRecord
   belongs_to :question
 
-  validates :title, :body, presence: true, length: { minimum: 6 }
+  validates :body, presence: true, length: { minimum: 6 }
 end
