225cdd9e8f7a6f518afe37fe0a78b4f14529f2cb
savchenko <mr.syava1@gmail.com>
2018-09-15 23:34:12 +0500

    Questions controller from screencast

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,15 @@ module Advanced
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+      g.test_framework :rspec,
+                       fixtures: true,
+                       view_spec: true,
+                       controller_spec: true,
+                       helper_specs: false,
+                       routing_specs: false,
+                       request_specs: false
+      g.fixture_replacement :factory_bot, dir: 'spec/factories'
+    end
   end
 end
