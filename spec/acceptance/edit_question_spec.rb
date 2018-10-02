6329aa218af0cbbc8044c1d92902a429f03164af
Sergey <5695559@mail.ru>
2018-10-02 22:22:44 +0500

    Editing question

---
+++ spec/acceptance/edit_question_spec.rb
@@ -0,0 +1,46 @@
+require_relative 'acceptance_helper'
+
+feature 'Question editing', %q{
+  In order to fix mistake
+  As an author of question
+  I'd like to be able to edit my question
+} do
+  given(:user) { create(:user) }
+  given(:user2) { create(:user) }
+  given!(:question) { create(:question, user: user) }
+ 
+  scenario "Un-authenticated user want to edit question" do
+    visit question_path(question)
+    expect(page).to_not have_link 'Edit question'
+  end
+
+  describe "Authenticated user" do
+    before do
+      sign_in(user)
+      visit question_path(question)
+    end
+
+    scenario "sees link to edit" do
+      expect(page).to have_link 'Edit question'
+    end
+
+    scenario 'try to edit his answer', js: true do
+      click_on 'Edit question'
+      within '.question' do
+        fill_in "New title",	with: "New question title"
+        fill_in "New body",	with: "New question body"
+        click_on 'Save'
+        expect(page).to_not have_content question.body
+        expect(page).to have_content "New question title"
+        expect(page).to have_content "New question body"
+        expect(page).to_not have_selector 'textarea'
+      end
+    end
+  end
+
+  scenario "Non author can't sees link to edit" do
+    sign_in(user2)
+    visit question_path(question)
+    expect(page).to_not have_link 'Edit question'
+  end
+end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -44,18 +44,6 @@ RSpec.describe QuestionsController, type: :controller do
     end
   end
 
-  describe 'GET #edit' do
-    before { get :edit, params: { id: question } }
-
-    it 'assigns the requested Question to @question' do
-      expect(assigns(:question)).to eq question
-    end
-
-    it 'render template "edit"' do
-      expect(response).to render_template :edit
-    end
-  end
-
   describe 'POST #create' do
     context 'with valid attributes' do
       it 'link new Question with current user' do
@@ -83,24 +71,24 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'PATCH #update' do
     context 'with valid attributes' do
       it 'assigns the requested question to @question' do
-        patch :update, params: { id: question, question: attributes_for(:question) }
+        patch :update, params: { id: question, question: attributes_for(:question) }, format: :js
         expect(assigns(:question)).to eq question
       end
       it 'changes question attributes' do
-        patch :update, params: { id: question, question: { title: 'new_title', body: 'new_body' } }
+        patch :update, params: { id: question, question: { title: 'new_title', body: 'new_body' } }, format: :js
         question.reload
         expect(question.title).to eq 'new_title'
         expect(question.body).to eq 'new_body'
       end
 
       it 'redirect to the updated question' do
-        patch :update, params: { id: question, question: attributes_for(:question) }
-        expect(response).to redirect_to question
+        patch :update, params: { id: question, question: attributes_for(:question) }, format: :js
+        expect(response).to render_template :update
       end
     end
 
     context 'with invalid attributes' do
-      before { patch :update, params: { id: question, question: { title: 'new_title', body: nil } } }
+      before { patch :update, params: { id: question, question: { title: 'new_title', body: nil } }, format: :js }
 
       it 'does not change question attributes' do
         question.reload
@@ -109,7 +97,7 @@ RSpec.describe QuestionsController, type: :controller do
       end
 
       it 're-render "edit" template' do
-        expect(response).to render_template :edit
+        expect(response).to render_template :update
       end
     end
   end
