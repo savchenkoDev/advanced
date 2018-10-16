fbad26d4f1f77c9e4821736706d46d6ca9bedacf
Sergey <5695559@mail.ru>
2018-10-17 00:48:22 +0500

    User's model accept Comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :answers
   has_many :votes
+  has_many :comments
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,7 @@ RSpec.describe User do
   it { should have_many(:questions).dependent(:destroy) }
   it { should have_many(:answers) }
   it { should have_many(:votes) }
+  it { should have_many(:comments) }
   
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
