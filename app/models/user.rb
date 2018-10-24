97085227352fbae8d24451ece7138dba13fd589a
Sergey <5695559@mail.ru>
2018-10-25 02:41:26 +0500

    Facebook OAuth

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,26 @@ class User < ApplicationRecord
   has_many :answers
   has_many :votes
   has_many :comments
+  has_many :authorizations
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook]
+
+  def self.find_for_oauth(auth)
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    return authorization.user if authorization
+    
+    email = auth.info[:email]
+    user = User.where(email: email).first
+    unless user
+      password = Devise.friendly_token[0, 20]
+      user = User.create!(email: email, password: password, password_confirmation: password)
+    end
+    user.authorizations.create(provider: auth.provider, uid: auth.uid)
+    user
+  end
 
   def author_of?(entity)
     entity.user_id == self.id
@@ -17,7 +32,6 @@ class User < ApplicationRecord
     self.votes.where(votable: entity).first
   end
   
-
   def voted?(entity)
     entity.votes.where(user_id: self.id).exists?
   end
