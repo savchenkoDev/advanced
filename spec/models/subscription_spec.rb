658eed46450a1c52c5332643d1d34edceff01d49
Sergey <5695559@mail.ru>
2018-11-11 18:27:03 +0500

    Subscriptions model

---
+++ spec/models/subscription_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Subscription, type: :model do
+  describe 'associations' do
+    it { should belong_to :user  } 
+    it { should belong_to :question  }  
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :user_id }
+    it { should validate_presence_of :question_id }
+    it { should validate_uniqueness_of(:user_id).scoped_to(:question_id) }
+  end
+  
+  
+end
