6cdb4109c908e4762f246ad93ee891fd401c5f94
Sergey <5695559@mail.ru>
2018-11-13 23:57:22 +0500

    Subscription model refacrot (with spec)

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,6 +2,5 @@ class Subscription < ApplicationRecord
   belongs_to :user
   belongs_to :question
 
-  validates :user_id, :question_id, presence: true
   validates :user_id, uniqueness: { scope: [:question_id] }
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -7,8 +7,6 @@ RSpec.describe Subscription, type: :model do
   end
 
   describe 'validations' do
-    it { should validate_presence_of :user_id }
-    it { should validate_presence_of :question_id }
     it { should validate_uniqueness_of(:user_id).scoped_to(:question_id) }
   end
   
