1fd4cfc3dd341b098de30bfa196579363e253aa3
Sergey <5695559@mail.ru>
2018-11-13 23:56:54 +0500

    Send notify refactor

--- app/jobs/new_answer_notification_job.rb
+++ app/jobs/new_answer_notification_job.rb
@@ -3,8 +3,8 @@ class NewAnswerNotificationJob < ApplicationJob
 
   def perform(answer)
     question = answer.question
-    question.subscribers.each do |user|
-      NewAnswerNotificationMailer.subscribers_notification(answer, user).deliver_now
+    question.subscribers.find_each do |user|
+      NewAnswerNotificationMailer.subscribers_notification(answer, user).deliver_later
     end
   end
 end
